19927261ad32a765f1512ae9d13acd599eafe331
Harvey <410765484@qq.com>
2025-03-03 20:29:38 +0800

    response stream

--- harvey-ai/src/main/java/com/harvey/ai/config/ChatConfig.java
+++ harvey-ai/src/main/java/com/harvey/ai/config/ChatConfig.java
@@ -1,6 +1,7 @@
 package com.harvey.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +15,14 @@ public class ChatConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
         return builder.defaultSystem("你是一个智能机器人，你的名字叫小杨。").build();
+    }
+
+    /**
+     * 基于内存的方式实现对话记忆
+     * @return
+     */
+    @Bean
+    InMemoryChatMemory inMemoryChatMemory() {
+        return new InMemoryChatMemory();
     }
 }
--- harvey-ai/src/main/java/com/harvey/ai/controller/ChatBotController.java
+++ harvey-ai/src/main/java/com/harvey/ai/controller/ChatBotController.java
@@ -2,10 +2,13 @@ package com.harvey.ai.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 /**
  * @author Harvey
@@ -17,12 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatBotController {
     private final ChatClient chatClient;
 
-    @PostMapping(value = "/chat")
-    public String chat(@RequestBody String message) {
+    @PostMapping(value = "/chat/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<String>> chat(@RequestBody String message) {
         return chatClient.prompt()
                 .user(message)
-                .call()
-                .content();
+                .stream()
+                // 添加事件区分正常回答消息(message)和错误消息(error)
+                .content().map(content -> ServerSentEvent.builder(content).event("message").build())
+                // 添加问题回答结束标识
+                .concatWithValues(ServerSentEvent.builder("[DONE]").build())
+                .onErrorResume(e -> Flux.just(ServerSentEvent.builder("Error: " + e.getMessage()).event("error").build()));
     }
 
 }
